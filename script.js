df204c11b19ca3470e41f6d583f0fcd53b3e0203
Longitude <144949360+Manny00001@users.noreply.github.com>
2026-04-29 15:34:03 -0400

    Remove selectLesson function from script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-function selectLesson(number) {
-  const lessons = document.querySelectorAll(".lesson");
 
-  lessons.forEach((lesson) => {
-    lesson.classList.remove("active");
-  });
-
-  const selected = lessons[number - 1];
-
-  if (!selected || selected.classList.contains("locked")) {
-    return;
-  }
-
-  selected.classList.add("active");
-}
