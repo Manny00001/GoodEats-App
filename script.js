685c076bca798dc05db4b4afece73a892dde74e2
Longitude <144949360+Manny00001@users.noreply.github.com>
2026-04-29 19:01:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const feed = document.getElementById("feed");
 
+feed.addEventListener("scroll", () => {
+  const maxScroll = feed.scrollHeight - feed.clientHeight;
+
+  if (feed.scrollTop >= maxScroll - 5) {
+    setTimeout(() => {
+      feed.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    }, 250);
+  }
+});
